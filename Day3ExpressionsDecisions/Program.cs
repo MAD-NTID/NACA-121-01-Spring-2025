7bd5629fb153d847fca88ba0b184427e9caee027
Michael Berrios <mxbics@rit.edu>
2025-01-17 11:41:33 -0500

    Day 3 Demo

---
+++ Day3ExpressionsDecisions/Program.cs
@@ -0,0 +1,44 @@
+﻿// Console.Write("Enter your name: ");
+
+// Console will stop here and wait for user input
+// string name = Console.ReadLine();
+
+// Console.Write("Enter your age: ");
+// int age = int.Parse(Console.ReadLine());
+// string ageString = age.ToString();  // this is explicit conversion
+
+// because of interpolation {age} is implicitly converted to string
+// Console.WriteLine($"Your name is {name} and you are {age} years old");
+
+// You can initialize multiple variables of the same type in one STATEMENT
+// int a = 1, 
+//     b = 2, 
+//     c = 3, 
+//     d = 4;
+
+// int a2 = 1;
+// int b2 = 2;
+// int c2 = 3;
+// int d2 = 4;
+
+// Let's compare prefix and postfix
+// int value = 5;
+
+// // what is the value now before I prefix increment
+// // Console.WriteLine(++value);
+
+// // What is the value now after I postfix increment
+// Console.WriteLine(value++);
+
+// // What's the value after?
+// Console.WriteLine(value);
+
+bool cold = false;
+
+if(cold) 
+    // if only one statement after if, don't need curly brackets
+    Console.WriteLine("Wear a coat");
+else 
+{
+    Console.WriteLine("Wear shorts");
+}
---
+++ Day3GroupExercise1/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Justin works at a car dealership.  He gets a commission on his car sales.
+What we need is a tool to help us calculate Justin’s commission.
+
+Start Here
+Justin gets a 7% commission of the total sale
+Justin only gets the commission if the sale is $10,000 or more
+Examples:
+If Justin sells a car for $15,000 his commission is $1,050
+If Justin sells a car for $7,000 his commission is $0
+*/
+const double COMMISSION = .07;
+
+Console.Write("Enter the Price of the car: ");
+
+double carPrice = double.Parse(Console.ReadLine());
+double earnedCommission = 0;
+
+if(carPrice >= 10000)
+    earnedCommission = carPrice * COMMISSION;
+
+Console.WriteLine($"Justin sold a car for {carPrice:C} and earned {earnedCommission:C}");

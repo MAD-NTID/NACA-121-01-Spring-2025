b757c15deb04689ceaa1a8e15b1dcc63e7c60468
Michael Berrios <mxbics@rit.edu>
2025-02-03 10:33:33 -0500

    Demonstrate differences overloading

--- Day08GettersSettersMethods/Program.cs
+++ Day08GettersSettersMethods/Program.cs
@@ -5,3 +5,17 @@ Console.WriteLine($"Person Created: {person.GetName()}");
 Console.WriteLine($"Age is: {person.GetAge()}");
 Console.WriteLine($"Weight is {person.GetWeight()} and Height is {person.GetHeight()}");
 Console.WriteLine($"The last four of SSN is {person.GetSSN()}");
+
+Console.WriteLine();
+
+Person person2 = new Person();
+person2.SetFirstName("Tony Stark");
+person2.SetAge(32);
+person2.SetWeight(150.55);
+person2.SetHeight(5.72);
+person2.SetSSN("123-12-1234");
+
+Console.WriteLine($"Person Created: {person2.GetName()}");
+Console.WriteLine($"Age is: {person2.GetAge()}");
+Console.WriteLine($"Weight is {person2.GetWeight()} and Height is {person2.GetHeight()}");
+Console.WriteLine($"The last four of SSN is {person2.GetSSN()}");

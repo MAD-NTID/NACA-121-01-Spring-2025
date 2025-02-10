799882714e84359a802fdeca64e999c7d4e0bdb5
Michael Berrios <mxbics@rit.edu>
2025-02-10 13:18:47 -0500

    Almost completed with the midterm review. Just checking in a couple of things with validation

--- StarFleet/Program.cs
+++ StarFleet/Program.cs
@@ -1,2 +1,118 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Ship ship = new Ship("Voyager", 3000);
+
+// Menu options for the user
+string[] menus = {
+    "Add Alien to ship",
+    "Fly",
+    "Fly at specific speed",
+    "Remove an alien from the ship",
+    "Show ship information",
+    "Exit"
+};
+
+while (true)
+{
+    try
+    {
+        int selection = Selection();
+        switch (selection)
+        {
+            case 1:
+                // Adding an Alien to the ship
+                Alien alien;
+                bool added = false;
+                string name;
+                
+                do
+                {
+                    Console.Write("Name: ");
+                    name = Console.ReadLine();
+                    
+                    Console.Write("Role: ");
+                    string role = Console.ReadLine();
+                    
+                    alien = new Alien(name, role);
+                    added = ship.Add(alien);
+                    
+                    if (!added)
+                        Console.WriteLine("Only 1 Captain or Chief Officer allowed!");
+                    
+                } while (!added);
+                
+                Console.WriteLine($"Alien {alien} added!");
+                break;
+
+            case 2:
+                // Fly the ship at default speed
+                ship.Fly();
+                Console.WriteLine($"{ship.ShipName} flew {ship.Distance} interstellar distance at {ship.WarpSpeed} warp speed");
+                break;
+            
+            case 3:
+                // Fly the ship at a specific warp speed
+                Console.Write("Enter the warp speed (multiple of 3): ");
+                
+                if (!int.TryParse(Console.ReadLine(), out int speed) || speed % 3 != 0)
+                {
+                    Console.WriteLine("Invalid speed! Speed must be an integer multiple of 3!\nShip did not fly!");
+                }
+                else
+                {
+                    ship.Fly(speed);
+                    Console.WriteLine($"{ship.ShipName} flew {ship.Distance} interstellar distance at {ship.WarpSpeed} warp speed");
+                }
+                break;
+
+            case 4:
+                // Remove an Alien from the ship
+                Console.Write("Alien's name: ");
+                name = Console.ReadLine();
+                bool removed = ship.Remove(name);
+                
+                if (!removed)
+                {
+                    Console.WriteLine($"{name} doesn't exist!");
+                }
+                else
+                {
+                    Console.WriteLine($"{name} successfully removed");
+                }
+                break;
+
+            case 5:
+                // Display ship information
+                Console.Write(ship);
+                break;
+            
+            case 6:
+                // Exit the program
+                Console.WriteLine("Thank you for exploring space with us!");
+                Environment.Exit(0);
+                break;
+            
+            default:
+                Console.WriteLine("Invalid selection!");
+                break;
+        }
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Invalid selection. Input must be a digit!");
+    }
+}
+
+// Displays the menu and retrieves user selection
+int Selection()
+{
+    Console.WriteLine("Starfleet Federation");
+    Console.WriteLine();
+    Console.WriteLine("Menu\n================");
+    
+    for (int i = 1; i <= menus.Length; i++)
+    {
+        Console.WriteLine($"{i} - {menus[i - 1]}");
+    }
+    
+    Console.Write("Selection: ");
+    return int.Parse(Console.ReadLine());
+}
--- StarFleet/Ship.cs
+++ StarFleet/Ship.cs
@@ -147,7 +147,7 @@ class Ship
         string crewListing = "";
         for(int i = 0; i < aliens.Length; i++)
         {
-            if(crewListing[i] != null)
+            if(aliens[i] != null)
             {
                 crewListing += $"Name: {aliens[i].Name} Role: {aliens[i].Role}\n";
             }

14b07fe8dd57b4af43c04217f0c9635f0c7269a0
Michael Berrios <mxbics@rit.edu>
2025-03-26 11:54:59 -0400

    Completed Node and Stack Demo

---
+++ Day27DataStructureINodeStack/Program.cs
@@ -0,0 +1,12 @@
+﻿Node<string> Michael = 
+    new Node<string>("Michael", 
+        new Node<string>("Yarin", 
+            new Node<string>("Amber", 
+                new Node<string>("Nai", 
+                    new Node<string>("Usfer")
+                    )
+                )
+            )
+        );
+
+Console.WriteLine(Michael);

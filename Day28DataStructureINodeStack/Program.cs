0057449e6d52306d62e0a6417a391a59868bc7f2
Michael Berrios <mxbics@rit.edu>
2025-03-28 10:48:26 -0400

    Added IEnumerator and Foreach

--- Day28DataStructureINodeStack/Program.cs
+++ Day28DataStructureINodeStack/Program.cs
@@ -1,12 +1,27 @@
-﻿Node<string> Michael = 
-    new Node<string>("Michael", 
-        new Node<string>("Yarin", 
-            new Node<string>("Amber", 
-                new Node<string>("Nai", 
-                    new Node<string>("Usfer")
-                    )
-                )
-            )
-        );
+﻿// Node<string> Michael = 
+//     new Node<string>("Michael", 
+//         new Node<string>("Yarin", 
+//             new Node<string>("Amber", 
+//                 new Node<string>("Nai", 
+//                     new Node<string>("Usfer")
+//                     )
+//                 )
+//             )
+//         );
 
-Console.WriteLine(Michael);
+// Console.WriteLine(Michael);
+
+Stack<string> people = new Stack<string>();
+people.Push("Michael");
+people.Push("Yarin");
+people.Push("Amber");
+people.Push("Nai");
+people.Push("Usfer");
+
+Console.WriteLine(people);
+
+// I can also use foreach because I implemented IEnumerator
+foreach(var person in people)
+{
+    Console.WriteLine(person);
+}
